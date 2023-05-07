12c5ad2fa74083a1597a9ea1301f0bc386f59662
Asha Saini <59530575+asha-saini06@users.noreply.github.com>
2023-05-07 11:55:26 +0530

    Added sql file and setup info

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ the-daily-drip
 SETUP:
 
 1. Start the Apache and MySQL modules using the XAMPP controller.
-2. Open the phpMyAdmin and create a database "lifestylestore".
-3. Import the lifestylestore.sql file present in the folder.
-4. Open the htdocs folder in the xampp folder. Copy paste the folder lifestylestore.
-5. Open the browser (chrome), type localhost/lifestylestore and you should see the index page of the website.
+2. Open the phpMyAdmin and create a database "contact_db".
+3. Import the contact_db.sql file present in the folder.
+4. Open the htdocs folder in the xampp folder. Copy paste the folder thedailydrip.
+5. Open the browser (chrome), type localhost/thedailydrip and you should see the index page of the website.
---
+++ contact_db.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 07, 2023 at 08:20 AM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.1.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `contact_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contact_form`
+--
+
+CREATE TABLE `contact_form` (
+  `id` int(100) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `number` varchar(10) NOT NULL,
+  `guests` varchar(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `contact_form`
+--
+
+INSERT INTO `contact_form` (`id`, `name`, `number`, `guests`) VALUES
+(1, 'alex', '1234567890', '2'),
+(2, 'Ash', '7654983210', '1');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `contact_form`
+--
+ALTER TABLE `contact_form`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `contact_form`
+--
+ALTER TABLE `contact_form`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
